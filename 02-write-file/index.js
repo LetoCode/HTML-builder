013a8ad88c777d54da8b1944577031b89db50b6b
LetoCode <pavolov.egor@gmail.com>
2022-10-31 18:31:32 +0300

    feat: solve task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require('path');
+
+const { exit, stdin, stdout } = process;
+stdout.write('Write something here \n > ');
+stdin.on('data', data => {
+   const filePath = path.resolve(__dirname, 'text.txt');
+   fs.appendFile(filePath, data, (err) => {
+      if (err) return console.error(err.message);
+      stdout.write('<--done!--> ');
+      exit();
+   })
+})
