a63216ebe07a320dcfc0843c4098553fd0917a6d
LetoCode <pavolov.egor@gmail.com>
2022-11-05 21:48:27 +0300

    fix: fix some bugs

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const { exit, stdin, stdout } = process;
+const { stdin, stdout } = process;
 stdout.write('Write something here \n > ');
 stdin.on('data', data => {
    const filePath = path.resolve(__dirname, 'text.txt');
    fs.appendFile(filePath, data, (err) => {
       if (err) return console.error(err.message);
-      stdout.write('<--done!--> ');
-      exit();
+      //stdout.write('<--done!--> ');
+      //exit();
    })
+})
+process.on('exit', () => {
+   stdout.write('Удачи в изучении Node.js!');
 })
