2b2bb375f419bfe8829db6b2ed2ea84e13f95c3e
LetoCode <pavolov.egor@gmail.com>
2022-11-06 11:20:24 +0300

    fix: fix task 2: exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const { stdin, stdout } = process;
+
+const { exit, stdin, stdout } = process;
 stdout.write('Write something here \n > ');
 stdin.on('data', data => {
    const filePath = path.resolve(__dirname, 'text.txt');
+   if (data.toString().slice(0, 4) === 'exit') {
+      stdout.write('Удачи в изучении Node.js!');
+      exit();
+   }
    fs.appendFile(filePath, data, (err) => {
       if (err) return console.error(err.message);
-      //stdout.write('<--done!--> ');
-      //exit();
    })
 })
-process.on('exit', () => {
+
+
+process.on('SIGINT', () => {
    stdout.write('Удачи в изучении Node.js!');
+   exit();
 })
